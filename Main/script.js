9c861c0d1922e3ab92fce0d3f8a2627fc3625fdc
Mazen Magdy <mazenmagdy190.mm@gmail.com>
2024-08-30 03:31:15 +0300

    Data types

--- Main/script.js
+++ Main/script.js
@@ -1,3 +1,10 @@
-var msg = "Hello World!";
+let num = 31;
+let str = "Yoo";
+let bol = true;
 
-console.log(msg);
+console.log(num);
+console.log(typeof num);
+console.log(str);
+console.log(typeof str);
+console.log(bol);
+console.log(typeof bol);
