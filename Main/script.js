3a777d2503099c32da101e4d947ecc4d84542463
Mazen Magdy <mazenmagdy190.mm@gmail.com>
2024-08-30 03:50:55 +0300

    adding undefined with the declaration and type

--- Main/script.js
+++ Main/script.js
@@ -1,10 +1,14 @@
+console.log("---")
+
+// ---
+
 // first declaration
 let num = 32;
-console.log("1st - " + num);
+console.log("1st ? " + num);
 
 // second declaration
 num = 21;
-console.log("2nd - " + num)
+console.log("2nd ? " + num)
 
 // ------
 console.log("---")
@@ -12,8 +16,27 @@ console.log("---")
 
 // first declaration
 let bol = true;
-console.log("1st - " + bol);
+console.log("1st ? " + bol);
 
 // second declaration
 bol = false;
-console.log("2nd - " + bol)
+console.log("2nd ? " + bol)
+
+// ------
+console.log("---")
+// ------
+
+// first declaration 
+let undef;
+console.log("1st ? " + undef)
+console.log("type ? " + typeof undef)
+
+undef = "def";
+console.log("2nd ? "+ undef)
+
+// ---
+
+console.log("---")
+
+
+
