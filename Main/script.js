2e8f137d77baf605e7f6e44b173a1344a9673bec
Mazen Magdy <mazenmagdy190.mm@gmail.com>
2024-08-31 17:46:02 +0300

    Math, Assignment and comparison Operators

--- Main/script.js
+++ Main/script.js
@@ -1,11 +1,36 @@
 // Operators 
-const now = 2024;
-const age1 = now - 2000;
-const age2 = now - 2005;
 
-console.log(age1, age2);
+// Math Operators 
+console.log("--- Math Operators ---")
 
-x = 10
-y = 5
+let x = 10;
+let y = 5;
 
-console.log(x / y, x * y, x ** y);
+console.log(x + y); // plus
+console.log(x - y); // minus
+console.log(x * y); // multiplication
+console.log(x / y); // division
+console.log(x % y); // Module
+console.log(x ** y); // Power
+
+// Assignment Operators
+console.log("--- Assignment Operators ---");
+
+x += 10; // x = x + 10
+console.log(x);
+
+x *= 2; // x = x * 2
+console.log(x);
+
+x++; // x = x + 1
+console.log(x);
+
+x--; // x = x - 1
+console.log(x);
+
+// Comparison Operators
+console.log("--- Comparison Operators ---");
+console.log(x > y); // x bigger than y ?
+console.log(x < y); // x smaller than y ?
+console.log(x > y * 8); // x bigger than y * 8 ?
+console.log(x >= y * 8); // x bigger than or equal y* 8 ?
