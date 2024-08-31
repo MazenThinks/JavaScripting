44638a83ef25e8b1a647b03a44490be8421f78f3
Mazen Magdy <mazenmagdy190.mm@gmail.com>
2024-08-31 17:12:18 +0300

    Simple Operators

--- Main/script.js
+++ Main/script.js
@@ -1,42 +1,11 @@
-console.log("--- Let ---");
+// Operators 
+const now = 2024;
+const age1 = now - 2000;
+const age2 = now - 2005;
 
-// let - value can be changed 
-let lett = 31;
-console.log("1st Value ? " + lett)
+console.log(age1, age2);
 
-lett = 32;
-console.log("2nd value ? " + lett);
+x = 10
+y = 5
 
-// ---
-console.log("--- Var ---");
-//---
-
-// Var - Same as Let 
-var varr = 31;
-console.log("1st Value ? " + varr);
-
-varr = 32;
-console.log("2nd Value ? " + varr);
-
-//---
-console.log("--- Const ---");
-//---
-
-// Const - unchangeable value 
-const constt = 31;
-console.log("1st Value ? " + constt);
-
-// it gives error
-// constt = 32; 
-// console.log("2nd Value ? " + constt);
-
-// ---
-console.log("--- Free Var ---");
-//---
-
-// free var 
-freeVar = 31;
-console.log("1st Value ? " + freeVar);
-
-freeVar = 32;
-console.log("2nd Value ? " + freeVar);
+console.log(x / y, x * y, x ** y);
